b30db6e66c679427f63ffc898ba587d0277ab57a
DESKTOP-OAK\mi <michal.sflap@gmail.com>
2018-07-04 23:41:13 +0200

    Add rewired, Add emotion babel plugin

--- src/Pages/Antd/index.js
+++ src/Pages/Antd/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import styled from 'react-emotion'
+import styled, { css } from 'react-emotion'
 import { DatePicker, version } from 'antd'
 import moment from 'moment'
 import 'antd/dist/antd.css'
 
 const DateFrom = styled('div')``;
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+const Test = css`
+  color: green;
+`
+
+const Test2 = styled.div`
+  ${Test}
+  font-size: 36px;
+`
+// this one is not working
+const CustomDateFrom = styled('DatePicker')`
+  color: magenta;
+`
 
 const DateFromAD = () => {
   return (
@@ -16,6 +29,10 @@ const DateFromAD = () => {
       </p>
       <DatePicker size="small" placeholder="Departure date"/>
       <DatePicker size="small" placeholder="Return date"/>
+      <Test>Czy działa babel plugin</Test>
+      <Test2>Czy działa babel plugin</Test2>
+      <CustomDateFrom size="small" defaultValue={moment.now()}/>
+      <DatePicker size="small"/>
     </div>
   )
 }
